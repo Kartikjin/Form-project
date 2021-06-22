70aad6a40a50c6632071f7579bf02af230de5dc5
Kartik Jindal <02kartikjind@gmail.com>
2021-06-22 22:51:50 +0530

    Updated Settings.py and Urls.py

--- user_form/urls.py
+++ user_form/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.UserFormView.as_view(), name="user-form"),
-    path('list/', views.UserListView.as_view(), name="user-form-list")
+    path('/user-form', views.UserFormView.as_view(), name="user-form"),
+    path('/user-form/list/', views.UserListView.as_view(), name="user-form-list")
 ]
