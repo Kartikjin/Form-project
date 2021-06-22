0eb526d94e86a7944c81876ea4a355422759e7be
Kartik Jindal <02kartikjind@gmail.com>
2021-06-22 23:49:38 +0530

    Updated Settings.py

--- internship_project/settings.py
+++ internship_project/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'internship_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "user-form",
     }
 }
 
